d4699069a19b2bae250db7a55db61052a40fdec0
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2023-04-25 18:27:59 +0200

    Added database helper class

---
+++ src/db/database.php
@@ -0,0 +1,40 @@
+<?php
+class DatabaseHelper
+{
+    private $db;
+
+    public function __construct($servername, $username, $password, $dbname, $port)
+    {
+        $this->db = new mysqli($servername, $username, $password, $dbname, $port);
+        if ($this->db->connect_error) {
+            die("Connection failed: " . $this->db->connect_error);
+        }
+    }
+
+    public function addLoginAttempt($username, $time)
+    {
+        //TODO: implementare
+    }
+
+    public function getUser($email)
+    {
+        // Usando statement sql 'prepared' non sarà possibile attuare un attacco di tipo SQL injection.
+        if ($stmt = $this->db->prepare("SELECT username, password FROM persona WHERE email = ? LIMIT 1")) {
+            $stmt->bind_param('s', $email);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            return $result->fetch_all(MYSQLI_ASSOC);
+        }
+    }
+
+    public function getPassword($username)
+    {
+        // Usando statement sql 'prepared' non sarà possibile attuare un attacco di tipo SQL injection.
+        if ($stmt = $this->db->prepare("SELECT password FROM persona WHERE username = ? LIMIT 1")) {
+            $stmt->bind_param('s', $username);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            return $result->fetch_all(MYSQLI_ASSOC);
+        }
+    }
+}
