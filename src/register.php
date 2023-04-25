8545c77f62e1191463d11379a3c6cbd61ca40cf5
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2023-04-25 18:58:54 +0200

    Added register page and binded to base-outside

---
+++ src/register.php
@@ -0,0 +1,7 @@
+<?php
+require_once("bootstrap.php");
+
+$templateParams["page"] = "register-form.php";
+require_once("template/base-outside.php");
+
+?>
---
+++ src/template/register-form.php
@@ -0,0 +1,42 @@
+<section class="text-center text-lg-start">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="card-body text-center">
+                    <h2 class="fw-bold">Iscriviti adesso</h2>
+                    <form>
+                        <!-- Username input -->
+                        <div class="form-outline">
+                            <label class="form-label">Nome Utente</label>
+                            <input type="text" placeholder="username" class="form-control" />
+                        </div>
+
+                        <!-- Email input -->
+                        <div class="form-outline">
+                            <label class="form-label">Indirizzo Email</label>
+                            <input type="email" placeholder="esempio@dominio.com" class="form-control" />
+                        </div>
+
+                        <!-- Password input -->
+                        <div class="form-outline">
+                            <label class="form-label">Password</label>
+                            <input type="password" placeholder="Inserire la password" class="form-control" />
+                        </div>
+
+                        <!-- Password confirm -->
+                        <div class="form-outline">
+                            <label class="form-label">Conferma Password</label>
+                            <input type="password" placeholder="Ripetere la password" class="form-control" />
+                        </div>
+
+                        <!-- Submit button -->
+                        <button type="submit" class="btn btn-primary btn-block mb-4">Registrati</button>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <h1 class="text-center">Placeholder immagine</h1>
+            </div>
+        </div>
+    </div>
+</section>
