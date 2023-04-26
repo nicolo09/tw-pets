af160640237892c608a73924379f57cbcb087ac1
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2023-04-26 17:50:54 +0200

    Fixed imagePreview method to check file extension

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,11 @@
 /* When selecting an image this shows its preview on 
  * an img tag with id=#imgPreview */
 function imagePreview(input){
-    if (input.files && input.files[0]) {
+    if (input.files && input.files[0] && input.files[0].name.match(/\.(jpg|jpeg|png|gif)$/i)) {
         var reader = new FileReader();
-        console.log("ciao")
         reader.onload = function (e) {
             $('#imgPreview')
                 .attr('src', e.target.result)
-                .width(150)
-                .height(200);
         };
 
         reader.readAsDataURL(input.files[0]);
