e4bb35a84c4e2bfed480942299ff89062941c9d3
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2023-04-26 10:26:11 +0200

    Added newline at end of files

--- src/register.php
+++ src/register.php
@@ -1,6 +1,14 @@
 <?php
 require_once("bootstrap.php");
 
+if (isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["email"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $email = $_POST["email"];
+
+    
+}
+
 $templateParams["page"] = "register-form.php";
 require_once("template/base-outside.php");
 
