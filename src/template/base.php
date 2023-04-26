13dba8750925d7612cf6117cbf7b5b71793b77e3
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2023-04-26 12:26:16 +0200

    Added style.css and added title tag to base

--- src/template/base.php
+++ src/template/base.php
@@ -9,6 +9,12 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
+    <title><?php
+    if (isset($templateParams["title"])) {
+        require_once($templateParams["title"]);
+    }
+    ?></title>
+
 </head>
 
 <body>
