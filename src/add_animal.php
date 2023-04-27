4c19d5f3635db139381e03efcc188f688cfc611c
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2023-04-27 16:55:31 +0200

    Enabled login check in add_animal.php

--- src/add_animal.php
+++ src/add_animal.php
@@ -1,15 +1,15 @@
 <?php
 require_once("bootstrap.php");
 
-/*if(!login_check($dbh)){
+if(!login_check($dbh)){
     header("Location: login.php");
     exit;
-}*/
+}
 
 if(isset($_POST["username"], $_POST["type"]) && !empty($_POST["username"]) && !empty($_POST["type"])){
     $animal = htmlspecialchars($_POST["username"]);
     $type = htmlspecialchars($_POST["type"]);
-    if(isset($_FILES["imgprofile"]) && !empty($_FILES["imgprofile"])){
+    if(!empty($_FILES["imgprofile"]["name"])){
         list($result, $msg) = uploadImage(IMG_DIR, $_FILES["imgprofile"]);
         if($result != 0){
             $img = $msg;
@@ -41,7 +41,7 @@ if(isset($_POST["username"], $_POST["type"]) && !empty($_POST["username"]) && !e
 
 $templateParams["page"] = "add-animal-form.php";
 $templateParams["img"] = "img/default_pet_image.png";
-//$templateParams["user"] = $_SESSION['username']; 
+//$templateParams["user"] = $_SESSION['username']; TODO use it on add-animal-form.php
 
 require_once("template/base.php");
 ?>
