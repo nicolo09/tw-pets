d1f4a1f7d43187f16c53a38ab0c8c24e9c80e724
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2023-04-27 15:34:43 +0200

    Reformat

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 /* When selecting an image this shows its preview on 
  * an img tag with id=#imgPreview */
-function imagePreview(input){
+function imagePreview(input) {
     if (input.files && input.files[0] && input.files[0].name.match(/\.(jpg|jpeg|png|gif)$/i)) {
         var reader = new FileReader();
         reader.onload = function (e) {
