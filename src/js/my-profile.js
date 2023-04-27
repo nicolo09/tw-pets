508bdce4eb2f12cb531f3a549336c47d241f1e82
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2023-04-27 11:22:26 +0200

    Implemented logout confirmation via jquery

---
+++ src/js/my-profile.js
@@ -0,0 +1,17 @@
+const logoutButton = document.querySelector('#logout-button');
+
+logoutButton.addEventListener('click', () => {
+    // Ask for confirmation
+    $('#buttons-div').empty();
+    $('#buttons-div').append(`
+        <h1 class="text-center">Sei sicuro di voler uscire?</h1>
+        <button id="yes-button" class="btn btn-primary">Si</button>
+        <button id="no-button" class="btn btn-primary">No</button>
+    `);
+    $('#yes-button').on("click", function () {
+        window.location.href = 'logout.php';
+    });
+    $('#no-button').on("click", function () {
+        window.location.href = 'tab-profile.php';
+    });
+});
