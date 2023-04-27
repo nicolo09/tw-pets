c54c0f6aa2b8ae9ff70f6f756cb94fc578160e00
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2023-04-27 16:52:58 +0200

    Moved jQuery script declaration to head, removed jQuery UI

--- src/template/base.php
+++ src/template/base.php
@@ -3,18 +3,19 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    
     <link rel="stylesheet" type="text/css" href="../css/style.css" />
-
+    
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-
+    
+    <!-- jQuery script -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    
     <title><?php
     if (isset($templateParams["title"])) {
         echo($templateParams["title"]);
     }
     ?></title>
-
 </head>
 
 <body>
@@ -35,8 +36,6 @@
     }
     ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-<script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-<script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.0/jquery-ui.min.js"></script>
 <script src="/js/script.js" type="text/javascript"></script>
 </body>
 </html>
