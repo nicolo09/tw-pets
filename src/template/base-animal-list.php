bc20b198182db6e2e6c75d181c1ae30775262d74
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2023-04-29 18:43:25 +0200

    Animal list grid differs between mobile and desktop

--- src/template/base-animal-list.php
+++ src/template/base-animal-list.php
@@ -1,18 +1,39 @@
 <h1 class="text-center">Animali di <?php echo $templateParams["user"]?></h1>
 <div class="d-grid gap-3">
+    <div class="mobile-view">
     <?php for($x = 0; $x < count($templateParams["animals"]); $x+=2): ?>
         <div class="row proPicContainer">
-            <?php for($y = 0; $y < 2 && ($x + $y) < count($templateParams["animals"]) ; $y++):?>
+            <?php for($y = 0; $y < 2; $y++):?>
                 <div class="col text-center">
+                    <?php if(isset($templateParams["animals"][$x + $y])): ?>
                     <img class="proPicBtn" 
                         src="<?php echo IMG_DIR.$templateParams["animals"][$x + $y]["immagine"]?>" 
                         alt="Profilo di <?php echo $templateParams["animals"][$x + $y]["username"]?>"
                         onclick='goToAnimal("<?php echo $templateParams["animals"][$x + $y]["username"]?>")'>
                     <label class="w-100 fs-4 fw-bolder"><?php echo $templateParams["animals"][$x + $y]["username"]?></label>
+                    <?php endif ?>
                 </div>
             <?php endfor;?>
-        </div>        
+        </div>
     <?php endfor; ?>
+    </div>
+    <div class="desktop-view">
+    <?php for($x = 0; $x < count($templateParams["animals"]); $x+=4): ?>
+        <div class="row proPicContainer">
+            <?php for($y = 0; $y < 4; $y++):?>
+                <div class="col text-center">
+                    <?php if(isset($templateParams["animals"][$x + $y])): ?>
+                    <img class="proPicBtn" 
+                        src="<?php echo IMG_DIR.$templateParams["animals"][$x + $y]["immagine"]?>" 
+                        alt="Profilo di <?php echo $templateParams["animals"][$x + $y]["username"]?>"
+                        onclick='goToAnimal("<?php echo $templateParams["animals"][$x + $y]["username"]?>")'>
+                    <label class="w-100 fs-4 fw-bolder"><?php echo $templateParams["animals"][$x + $y]["username"]?></label>
+                    <?php endif ?>
+                </div>
+            <?php endfor;?>
+        </div>
+    <?php endfor; ?>
+    </div>        
     <?php if($_SESSION["username"] == $templateParams["user"]):?>
     <button class="btn btn-primary" id="add-animal-button"><img src="/img/add_animal.svg">Aggiungi animale</button>
     <?php endif ?>
