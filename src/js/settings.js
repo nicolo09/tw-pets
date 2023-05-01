97359f72f9d2a37f2d769364159f0f0d5c555848
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2023-05-01 21:54:29 +0200

    Added javascript to populate setting page on loading and to change them

---
+++ src/js/settings.js
@@ -0,0 +1,33 @@
+//Retrieve settings as json object
+
+$.ajax({
+    url: 'profile-settings.php',
+    type: 'get',
+    data: {
+        'json': true
+    },
+    dataType: 'json',
+    success: function (response) {
+        for (var key in response) {
+            item = document.getElementById(key);
+            if (response.hasOwnProperty(key) && item != null) {
+                item.checked = response[key];
+                item.addEventListener('change', event => {
+                    $.ajax({
+                        url: 'profile-settings.php',
+                        type: 'post',
+                        data: { 'setting': event.target.id, 'value': event.target.checked },
+                        success: function (response) { 
+                            //TODO: Add success message and handle error
+                         }
+                    });
+                });
+                // Enable element
+                item.disabled = false;
+            }
+        }
+    }
+    //TODO: Handle error
+});
+
+
