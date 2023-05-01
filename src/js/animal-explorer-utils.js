34cadd0d3a419217acda732d564ec566196fad7d
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2023-05-01 15:12:59 +0200

    Added function to add eventListener to manage-buttons in animal-explorer-utils.js

--- src/js/animal-explorer-utils.js
+++ src/js/animal-explorer-utils.js
@@ -2,8 +2,17 @@ document.querySelector('#add-animal-button').addEventListener('click', () => {
     window.location.href = 'profile-add-animal.php';
 });
 
+let buttons = document.getElementsByClassName("manage-button");
+for (var i = 0; i < buttons.length; i++){
+    buttons[i].myParam = buttons[i].id.split("_")[1];
+    buttons[i].addEventListener('click', goToEditAnimal, false);
+};
+
+function goToEditAnimal(evt) {
+    window.location.href = 'profile-add-animal.php?animal=' + evt.currentTarget.myParam;
+};
 
 function goToAnimal(animal_username){
     //location.href = ""; //TODO go to animal page
-    console.log(animal_username)
+    console.log(animal_username);
 };
