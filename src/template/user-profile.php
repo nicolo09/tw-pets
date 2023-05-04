585c8548a378ce9bcfe763c038370f1af9f8c792
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2023-05-04 15:51:22 +0200

    Fixed line being too high, added margin between buttons, added odd image handling

--- src/template/user-profile.php
+++ src/template/user-profile.php
@@ -1,23 +1,23 @@
-<div class="container-fluid text-center mb-2">
+<div class="container-fluid text-center pb-2">
     <div class="row">
-        <img src="img/facebook-default-profile-pic.jpg" alt="Foto profilo di ginopino00" class="proPic col p-0 mt-2" />
-        <div class="col">
+        <img src="img/facebook-default-profile-pic.jpg" alt="Foto profilo di ginopino00" class="proPic col p-0 mt-2 d-flex justify-content-end" />
+        <div class="col p-0 w-25">
             <h1>ginopino00</h1>
             <h2>Persona/Coach di me stesso</h2>
             <p>Scopri come diventare milionario grazie al mio novo canale cryptocurrencyt telegram</p>
         </div>
         <!--Bottoni-->
-        <div class="border-bottom border-black text-center row w-100 g-0">
-            <button class="btn btn-outline-primary col ml-10"><img src="img/add-user.svg" alt="" class="w-50">Segui</button>
-            <button class="btn btn-outline-primary col ml-10"><img src="img/pets.svg" alt="" class="w-50">Animali</button>
-            <button class="btn btn-outline-primary col "><img src="img/groups.svg" alt="" class="w-50">Followers</button>
+        <div class="text-center row w-100 g-0">
+            <button class="btn btn-outline-primary col m-2"><img src="img/add-user.svg" alt="" class="w-50">Segui</button>
+            <button class="btn btn-outline-primary col m-2"><img src="img/pets.svg" alt="" class="w-50">Animali</button>
+            <button class="btn btn-outline-primary col m-2"><img src="img/groups.svg" alt="" class="w-50">Followers</button>
         </div>
     </div>
 
 </div>
 
 <!--Galleria immagini-->
-<div class="container-fluid g-0 w-100">
+<div class="container-fluid g-0 w-100 border-top border-black mt-3 pt-2">
     <div class="row w-100 g-0">
         <img src="img/dog_small.jpg" alt="cani sulla spiaggia" class="col w-50 p-0 border border-black"/>
         <img src="img/dog_small.jpg" alt="cani sulla spiaggia" class="col w-50 p-0 border border-black"/>
@@ -29,5 +29,9 @@
     <div class="row w-100 g-0">
         <img src="img/dog_small.jpg" alt="cani sulla spiaggia" class="col w-50 p-0 border border-black"/>
         <img src="img/cat.jpg" alt="gatto arancione" class="col w-50 p-0 border border-black"/>
+    </div>
+    <div class="row w-100 g-0">
+        <img src="img/dog_small.jpg" alt="cani sulla spiaggia" class="col w-50 p-0 border border-black"/>
+        <img src="#" alt="" class="col w-50 p-0 border border-black"/>
     </div>
 </div>
