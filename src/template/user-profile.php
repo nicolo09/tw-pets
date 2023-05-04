76281068634f1dbc88d48b29607e03ce9253041f
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2023-05-04 12:56:42 +0200

    Fixed centering issues with page, added grid for images

--- src/template/user-profile.php
+++ src/template/user-profile.php
@@ -1,21 +1,33 @@
-<div class="container-fluid">
+<div class="container-fluid text-center mb-2">
     <div class="row">
-        <img src="img/facebook-default-profile-pic.jpg" alt="Foto profilo di ginopino00" class="proPic col p-0" />
+        <img src="img/facebook-default-profile-pic.jpg" alt="Foto profilo di ginopino00" class="proPic col p-0 mt-2" />
         <div class="col">
             <h1>ginopino00</h1>
             <h2>Persona/Coach di me stesso</h2>
             <p>Scopri come diventare milionario grazie al mio novo canale cryptocurrencyt telegram</p>
         </div>
         <!--Bottoni-->
-        <div class="border-bottom border-black text-center row">
-            <button class="btn btn-outline-primary col"><img src="img/add-user.svg" alt="">Segui</button>
-            <button class="btn btn-outline-primary col"><img src="img/pets.svg" alt="">Animali</button>
-            <button class="btn btn-outline-primary col"><img src="img/groups.svg" alt="">Followers</button>
+        <div class="border-bottom border-black text-center row w-100 g-0">
+            <button class="btn btn-outline-primary col ml-10"><img src="img/add-user.svg" alt="" class="w-50">Segui</button>
+            <button class="btn btn-outline-primary col ml-10"><img src="img/pets.svg" alt="" class="w-50">Animali</button>
+            <button class="btn btn-outline-primary col "><img src="img/groups.svg" alt="" class="w-50">Followers</button>
         </div>
     </div>
 
 </div>
 
-<!--Linea-->
-<img src="img/dogs-small.jpg" alt="cani sulla spiaggia" />
-<img src="img/dogs-small.jpg" alt="cani sulla spiaggia" />
+<!--Galleria immagini-->
+<div class="container-fluid g-0 w-100">
+    <div class="row w-100 g-0">
+        <img src="img/dog_small.jpg" alt="cani sulla spiaggia" class="col w-50 p-0 border border-black"/>
+        <img src="img/dog_small.jpg" alt="cani sulla spiaggia" class="col w-50 p-0 border border-black"/>
+    </div>
+    <div class="row w-100 g-0">
+        <img src="img/dog_small.jpg" alt="cani sulla spiaggia" class="col w-50 p-0 border border-black"/>
+        <img src="img/dog_small.jpg" alt="cani sulla spiaggia" class="col w-50 p-0 border border-black"/>
+    </div>
+    <div class="row w-100 g-0">
+        <img src="img/dog_small.jpg" alt="cani sulla spiaggia" class="col w-50 p-0 border border-black"/>
+        <img src="img/cat.jpg" alt="gatto arancione" class="col w-50 p-0 border border-black"/>
+    </div>
+</div>
