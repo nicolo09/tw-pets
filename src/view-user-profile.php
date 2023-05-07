2cb225bbf60c79eda559f9eb468d07e96561ae97
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2023-05-07 18:27:04 +0200

    Added redirect to own profile if no username is given

--- src/view-user-profile.php
+++ src/view-user-profile.php
@@ -14,6 +14,9 @@ if (isset($_GET["username"])) {
     if (doesPersonUsernameExist($_GET["username"], $dbh) == 1) {
         $username = $_GET["username"];
     }
+}else{
+    //Se non hai settato username guardo tuo profilo
+    $username=getUserName($dbh);
 }
 
 
