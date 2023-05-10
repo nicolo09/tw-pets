a6d83a86e0f61a1964336d4c79c1115b66743378
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2023-05-10 11:59:47 +0200

    Added loader animation when retrieving new entries in full-search-results.php

--- src/template/full-search-results.php
+++ src/template/full-search-results.php
@@ -1,19 +1,25 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-12 col-lg-10 col-xl-8 mx-auto">
-            <div class="results-container">
+            <div class="results-container" id="container">
                 <h2 class="text-center fw-bold mb-3"><?php echo $templateParams["type"]?> risultanti per "<?php echo $templateParams["search"]?>": </h2>
                 <?php for($x = 0; $x < count($templateParams["results"]); $x++): ?> 
-                    <div class="card result-bar">
+                    <a href=#><div class="card result-bar"> <!-- TODO change href with profile link -->
                         <div class="card-body p-2">
-                            <div class="result-element">
-                                <img class="miniature" src="<?php echo IMG_DIR.$templateParams["results"][$x]["immagine"] ?>" alt="Immagine profilo di <?php echo $templateParams["results"][$x]["username"] ?>"/>
-                                <label class="fs-4 fw-bold"><?php echo $templateParams["results"][$x]["username"] ?></label>
+                            <div class="result-element"> <!-- img and label on same line to avoid empty space -->
+                                <img class="miniature" src="<?php echo IMG_DIR.$templateParams["results"][$x]["immagine"] ?>" alt="Immagine profilo di <?php echo $templateParams["results"][$x]["username"] ?>"/><label class="fs-4 fw-bold miniatureLabel"><?php echo $templateParams["results"][$x]["username"] ?></label>
                             </div>
                         </div>
-                    </div>
+                    </div></a>
                 <?php endfor; ?>
+            </div>
+            <div class="d-flex justify-content-center align-items-center mt-4" id="spinner">
+                <div class="spinner-border text-primary spinner-border-sm"
+                    role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
             </div>
         </div>
     </div>
 </div>
+<script src="js/search-results.js"></script>
