eb748849d37feedcdbe552e5274ae51f1097c096
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2023-05-17 18:45:30 +0200

    Removed old code, started duplicating like functions to handle save button

--- src/js/post.js
+++ src/js/post.js
@@ -6,23 +6,29 @@ const cards = document.querySelectorAll('[id^="post-card-"]');
 const id = [];
 //Se un post ha il like o no
 const likedID = {};
+const savedID = {};
 for (i = 0; i < cards.length; i++) {
     tmp = cards[i].id.split("-")[2];
     id.push(tmp);
     likedID[tmp] = false;
+    savedID[tmp] = false;
 }
 
 //Chiedo al db se i post hanno like o no
 id.forEach(element => getPostLiked(element));
+id.forEach(element => getPostSaved(element));
+
 //Metto la grafica dei bottoni
-id.forEach(element => styleButton(element));
+id.forEach(element => {
+    styleButtonLike(element);
+    styleButtonSave(element);
+    attachLike(element);
+    attachSave(element)
+});
 
-
-function styleButton(id){
+function styleButtonLike(id) {
     //TODO: Add n likes
     const buttonL = document.getElementById("like-post-card-" + id);
-    //TODO: Implement save button functionality
-    const buttonS = document.getElementById("save-post-card-" + id);
     if (buttonL != null) {
         if (likedID[id] == true) {
             //Post ha like
@@ -32,7 +38,19 @@ function styleButton(id){
             buttonL.innerHTML = '<img src="' + IMGDIR + 'thumb_up.svg" alt="" />Mi Piace ';
         }
     }
+}
 
+function styleButtonSave(id) {
+    const buttonS = document.getElementById("save-post-card-" + id);
+    if (buttonS != null) {
+        if (likedID[id] == true) {
+            //Post ha like
+            buttonS.innerHTML = '<img src="' + IMGDIR + 'star_filled.svg" alt="" />Salvato ';
+        } else {
+            //Post non ha like
+            buttonS.innerHTML = '<img src="' + IMGDIR + 'star.svg" alt="" />Salva ';
+        }
+    }
 }
 
 function getPostLiked(id) {
@@ -46,45 +64,45 @@ function getPostLiked(id) {
     });
 }
 
-
-
-document.getElementById("likeB").addEventListener('click', () => {
-    $.ajax({
-        method: "GET",
-        url: "like.php",
-        data: {
-            "id": id
-        },
-        success: function (response) {
-            //cambio grafica
-            //$("#likeB").
+function getPostSaved(id) {
+    const singleSave = fetch("tell-js-save.php?id=" + id).then((response) => {
+        if (!response.ok) {
+            throw new Error("Something went wrong!");
         }
+        return response.json();
+    }).then((data) => {
+        savedID[id] = data;
     });
-});
+}
 
-document.getElementById("saveB").addEventListener('click', () => {
-    window.location.href = 'save.php?id=' + id;
-});
+function attachLike(id) {
+    document.getElementById("like-post-card-" + id).addEventListener('click', () => {
+        $.ajax({
+            method: "GET",
+            url: "like.php",
+            data: {
+                "id": id
+            },
+            success: function (response) {
+                getPostLiked(id);
+                styleButtonLike(id);
+            }
+        });
+    });
+}
 
-//Questa funzione ritorna la stringa del parametro get
-//es stringa input: ?username=Mario
-//Ritorna Mario
-//es stringa type=animal
-//Ritorna animal
-function processString(totString) {
-    if (totString.length > 0) {
-        tmp = totString.split('?');
-        //Se ha un ? iniziale viene rimosso
-        index = -1;
-        if (tmp.length == 1) {
-            //Non c'era ? iniziale
-            index = 0;
-        } else {
-            //C'era ? e quindi il testo è nella seconda parte
-            index = 1;
-        }
-        substring = tmp[index].split('=');
-        //La prima parte di substring è la parte del parametro prima dell =
-        return substring[1];
-    }
+function attachSave(id) {
+    document.getElementById("save-post-card-" + id).addEventListener('click', () => {
+        $.ajax({
+            method: "GET",
+            url: "save.php",
+            data: {
+                "id": id
+            },
+            success: function (response) {
+                getPostSaved(id);
+                styleButtonSave(id);
+            }
+        });
+    });
 }
---
+++ src/tell-js-save.php
@@ -0,0 +1,11 @@
+<?php
+require_once("bootstrap.php");
+$id=-1;
+if(isset($_GET["id"])){
+    $id=$_GET["id"];
+}
+
+$like=isPostSavedBy($id, getUserName($dbh), $dbh);
+//This list is to be communicated to javascript to show images
+echo json_encode($like);
+?>
