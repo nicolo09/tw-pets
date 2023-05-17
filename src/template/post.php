f53e4bcc8e04323fc268cd769d78bd2a5a908652
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2023-05-17 12:33:52 +0200

    Changed styling for post page, formatting changes, added card footer, card header, removed empty div to print error messages

--- src/template/post.php
+++ src/template/post.php
@@ -1,8 +1,8 @@
 <!--Rettangolo-->
-<div class="card mx-auto col-12 col-lg-10 col-xl-8 border-black align-items-center justify-content-center" id="post-card-<?php if (isset($templateParams["id"])) {
-                                                                                                                                echo $templateParams["id"];
-                                                                                                                            } ?>">
-    <div class="row border-bottom border-black post-header">
+<div class="card mx-auto col-12 col-lg-10 col-xl-8 border-black align-items-center justify-content-center post" id="post-card-<?php if (isset($templateParams["id"])) {
+                                                                                                                                    echo $templateParams["id"];
+                                                                                                                                } ?>">
+    <div class="card-header row border-bottom border-black post-header p-1">
         <!--Utente che ha postato-->
         <div class="col p-0">
             <img class="pro-pic text-center img-fluid" src=<?php if (isset($templateParams["immagineprofilo"])) {
@@ -10,20 +10,20 @@
                                                             } else {
                                                                 echo IMG_DIR . "default.jpg";
                                                             } ?> alt=<?php if (isset($templateParams["username"])) {
-                                                                                    echo "Foto profilo di " . $templateParams["username"];
-                                                                                } else {
-                                                                                    echo "Foto profilo di utente non esistente";
-                                                                                } ?>>
-        </div>
-        <a class="col d-flex align-items-center" <?php if (isset($templateParams["username"])) {
-                                                        echo html_entity_decode('href=view-user-profile.php?username=' . $templateParams["username"] . '&type=person');
-                                                    } else {
-                                                        echo html_entity_decode('href=#');
-                                                    } ?>><?php if (isset($templateParams["username"])) {
+                                                                            echo "Foto profilo di " . $templateParams["username"];
+                                                                        } else {
+                                                                            echo "Foto profilo di utente non esistente";
+                                                                        } ?>>
+            <a class="align-items-center" <?php if (isset($templateParams["username"])) {
+                                                echo html_entity_decode('href=view-user-profile.php?username=' . $templateParams["username"] . '&type=person');
+                                            } else {
+                                                echo html_entity_decode('href=#');
+                                            } ?>><?php if (isset($templateParams["username"])) {
                                                                     echo $templateParams["username"];
                                                                 } else {
                                                                     echo "Utente non esiste";
                                                                 } ?></a>
+        </div>
     </div>
     <!--Immagine-->
     <img class="post-image w-100 pt-1 pb-1" src=<?php if (isset($templateParams["immagine"])) {
@@ -35,30 +35,31 @@
                                                             } else {
                                                                 echo "Alt non presente";
                                                             } ?>>
-    <div class="w-100 m-0 border-bottom border-top border-black d-flex justify-content-center g-0">
-        <!--Tasti-->
-        <div class="row w-50 g-0">
-            <button class="btn btn-outline btn-outline-primary col" id="likeB-<?php if (isset($templateParams["id"])) {
-                                                                                    echo $templateParams["id"];
-                                                                                } ?>">
+    <div class="card-footer w-100 p-0 m-0">
+        <div class="w-100 m-0 d-flex justify-content-center g-0 row">
+            <!--Tasti-->
+            <div class="col g-0 div-button-post">
+                <button class="btn btn-outline btn-outline-primary button-post w-100 align-middle" id="post-card-<?php if (isset($templateParams["id"])) {
+                                                                                                                        echo $templateParams["id"];
+                                                                                                                    } ?>">
+
+            </div>
+            <div class="col g-0 div-button-post">
+                <button class="btn btn-outline btn-outline-primary button-post w-100 align-middle" id="post-card-<?php if (isset($templateParams["id"])) {
+                                                                                                                        echo $templateParams["id"];
+                                                                                                                    } ?>">
+
+            </div>
         </div>
-        <div class="row w-50 g-0">
-            <button class="btn btn-outline btn-outline-primary col" id="saveB-<?php if (isset($templateParams["id"])) {
-                                                                                    echo $templateParams["id"];
-                                                                                } ?>">
+        <div class="text-left">
+            <p>
+                <?php if (isset($templateParams["descrizione"]) && isset($templateParams["username"])) {
+                    $tmp = $templateParams["username"] . ":" . $templateParams["descrizione"];
+                    echo $tmp;
+                    //TODO:Animali che ci sono
+                } ?>
+            </p>
         </div>
-    </div>
-    <!--Error messages are printed here-->
-    <div class="error">
-    </div>
-    <div class="text-left">
-        <p>
-            <?php if (isset($templateParams["descrizione"]) && isset($templateParams["username"])) {
-                $tmp = $templateParams["username"] . ":" . $templateParams["descrizione"];
-                echo $tmp;
-                //TODO:Animali che ci sono
-            } ?>
-        </p>
     </div>
 </div>
 <script src="js/post.js" type="text/javascript"></script>
