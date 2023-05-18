4ac5da081e86eb7a2dbbc301fed80559d5c8cf27
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2023-05-18 11:46:26 +0200

    Added notification type enum and max notifications const

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,6 +2,15 @@
 /* Path to image dir, to be used when uploading images */
 define("BASE_FOLDER", __DIR__ . "/");
 define("IMG_DIR", "img/");
+define("MAX_NOTIFICATIONS", 99);
+
+enum NotificationType {
+    case FOLLOW;
+    case LIKE;
+    case COMMENT;
+    case POST;
+    case MESSAGE;
+}
 
 require_once("utils/functions.php");
 sec_session_start();
