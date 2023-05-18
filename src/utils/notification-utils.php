dc4290fbbeb680ab44d6cbaf5b118bed08315ac3
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2023-05-18 11:47:11 +0200

    Added notification utils functions file

---
+++ src/utils/notification-utils.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Returns the thumbnail src for a notification
+ * @param $notification the notification
+ * @return array src and alt of the thumbnail
+ */
+function getNotificationThumbnail($notification){
+    //TODO: Implementare
+    return null;
+}
+
+/**
+ * Returns the message of a notification
+ * @param $notification the notification
+ * @return string the message
+ */
+function getNotificationMessage($notification){
+    //TODO: Implementare
+}
+
+/**
+ * Returns the href of a notification
+ * @param $notification the notification
+ * @return string the ref
+ */
+function getNotificationRef($notification){
+    //TODO: Implementare
+}
+
+/**
+ * Returns the type of a notification
+ * @param $notification the notification
+ * @return NotificationType the type
+ */
+function getNotificationType($notification){
+    //TODO: Implementare
+}
+
+?>
