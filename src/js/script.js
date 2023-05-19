e1eaca5c8d2c3646893c774b08552908fd08c179
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2023-05-19 11:57:30 +0200

    Added javascript to update notifications badge in profile tab

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,19 @@
-const IMG_DIR= "img/";
+const IMG_DIR = "img/";
 
-$.fn.select2.defaults.set( "theme", "bootstrap-5" );
+//Select 2 default theme
+$.fn.select2.defaults.set("theme", "bootstrap-5");
 
-// TODO: Update notification badge
-if (/*TODO: Ci sono notifiche*/ true) {
-    let notificationsNumber = 1;
-    $('#profile-nav-element').append(' <span class="badge bg-info">' + notificationsNumber + '</span><span class="visually-hidden">Nuove notifiche</span>');
-}
+//Notification badge on profile nav element
+$.ajax({
+    url: "profile-notifications.php",
+    type: "GET",
+    data: {
+        "number": "true"
+    },
+    dataType: "json",
+    success: function (data) {
+        if (data["count"] > 0) {
+            $('#profile-nav-element').append(' <span class="badge bg-info">' + data["count"] + (data["hasMore"] ? "+" : "") + '</span><span class="visually-hidden">Nuove notifiche</span>');
+        }
+    }
+});
