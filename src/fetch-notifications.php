35395ab458a4d17a7c98c72706281ccdddc6a17a
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2023-05-19 12:06:47 +0200

    Added php file to fetch notifications from js

---
+++ src/fetch-notifications.php
@@ -0,0 +1,20 @@
+<?php
+require_once("bootstrap.php");
+require_once("utils/notification-utils.php");
+
+if (!isUserLoggedIn($dbh)) {
+    header("Location: login.php");
+    exit;
+}
+
+if (isset($_GET["number"]) && isset($_GET["offset"])) {
+    $notifications = $dbh->getNotifications(getUserName($dbh), $_GET["number"], $_GET["offset"]);
+    foreach ($notifications as $notification) {
+        $thumbnail = getNotificationThumbnail($notification, $dbh);
+        require("template/notification.php");
+    }
+    exit;
+}
+else {
+    exit;
+}
