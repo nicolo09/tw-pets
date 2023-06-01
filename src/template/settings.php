05f4f5e5390b8236337e939848faa5c6b8816f27
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2023-06-01 12:11:49 +0200

    Changed setting checkboxes to switches and added roles for improved experience with assistive tecnologies

--- src/template/settings.php
+++ src/template/settings.php
@@ -41,22 +41,22 @@
                     <div class="list-group-item">
                         <div class="row align-items-center">
                             <div class="col">
-                                <label class="mb-0 fw-bolder" for="alert-new-access">Nuovi accessi</label>
+                                <label class="mb-0 fw-bolder form-check-label" for="alert-new-access">Nuovi accessi</label>
                                 <p class="text-muted mb-0">Notifiche su nuovi accessi effettuati al tuo account.</p>
                             </div>
-                            <div class="col-auto">
-                                <input type="checkbox" class="custom-control-input settings-item" id="alert-new-access" checked="false" disabled />
+                            <div class="col-auto form-check form-switch">
+                                <input type="checkbox" role="switch" class="custom-control-input settings-item form-check-input" id="alert-new-access" checked="false" disabled />
                             </div>
                         </div>
                     </div>
                     <div class="list-group-item">
                         <div class="row align-items-center">
                             <div class="col">
-                                <label class="mb-0 fw-bolder" for="alert-change-password">Cambio password/email</label>
+                                <label class="mb-0 fw-bolder form-check-label" for="alert-change-password">Cambio password/email</label>
                                 <p class="text-muted mb-0">Notifiche quando viene cambiata la password o l'email associate all'account.</p>
                             </div>
-                            <div class="col-auto">
-                                <input type="checkbox" class="custom-control-input settings-item" id="alert-change-password" checked="false" disabled />
+                            <div class="col-auto form-check form-switch">
+                                <input type="checkbox" role="switch" class="custom-control-input settings-item form-check-input" id="alert-change-password" checked="false" disabled />
                             </div>
                         </div>
                     </div>
@@ -69,44 +69,44 @@
                     <div class="list-group-item">
                         <div class="row align-items-center">
                             <div class="col">
-                                <label class="mb-0 fw-bolder" for="alert-likes">Mi piace</label>
+                                <label class="mb-0 fw-bolder form-check-label" for="alert-likes">Mi piace</label>
                                 <p class="text-muted mb-0">Notificami quando qualcuno mette mi piace a un mio post.</p>
                             </div>
-                            <div class="col-auto">
-                                <input type="checkbox" class="custom-control-input settings-item" id="alert-likes" checked="false" disabled />
+                            <div class="col-auto form-check form-switch">
+                                <input type="checkbox" role="switch" class="custom-control-input settings-item form-check-input" id="alert-likes" checked="false" disabled />
                             </div>
                         </div>
                     </div>
                     <div class="list-group-item">
                         <div class="row align-items-center">
                             <div class="col">
-                                <label class="mb-0 fw-bolder" for="alert-comments">Commenti</label>
+                                <label class="mb-0 fw-bolder form-check-label" for="alert-comments">Commenti</label>
                                 <p class="text-muted mb-0">Notificami quando qualcuno commenta un mio post.</p>
                             </div>
-                            <div class="col-auto">
-                                <input type="checkbox" class="custom-control-input settings-item" id="alert-comments" checked="false" disabled />
+                            <div class="col-auto form-check form-switch">
+                                <input type="checkbox" role="switch" class="custom-control-input settings-item form-check-input" id="alert-comments" checked="false" disabled />
                             </div>
                         </div>
                     </div>
                     <div class="list-group-item">
                         <div class="row align-items-center">
                             <div class="col">
-                                <label class="mb-0 fw-bolder" for="alert-follow">Followers</label>
+                                <label class="mb-0 fw-bolder form-check-label" for="alert-follow">Followers</label>
                                 <p class="text-muted mb-0">Notificami quando qualcuno inizia a seguirmi.</p>
                             </div>
-                            <div class="col-auto">
-                                <input type="checkbox" class="custom-control-input settings-item" id="alert-follow" checked="false" disabled />
+                            <div class="col-auto form-check form-switch">
+                                <input type="checkbox" role="switch" class="custom-control-input settings-item form-check-input" id="alert-follow" checked="false" disabled />
                             </div>
                         </div>
                     </div>
                     <div class="list-group-item">
                         <div class="row align-items-center">
                             <div class="col">
-                                <label class="mb-0 fw-bolder" for="alert-follow-animal">Followers animali</label>
+                                <label class="mb-0 fw-bolder form-check-label" for="alert-follow-animal">Followers animali</label>
                                 <p class="text-muted mb-0">Notificami quando qualcuno inizia a seguire un mio animale.</p>
                             </div>
-                            <div class="col-auto">
-                                <input type="checkbox" class="custom-control-input settings-item" id="alert-follow-animal" checked="false" disabled />
+                            <div class="col-auto form-check form-switch">
+                                <input type="checkbox" role="switch" class="custom-control-input settings-item form-check-input" id="alert-follow-animal" checked="false" disabled />
                             </div>
                         </div>
                     </div>
@@ -119,33 +119,33 @@
                     <div class="list-group-item">
                         <div class="row align-items-center">
                             <div class="col">
-                                <label class="mb-0 fw-bolder" for="alert-new-post-person">Post persone che seguo</label>
+                                <label class="mb-0 fw-bolder form-check-label" for="alert-new-post-person">Post persone che seguo</label>
                                 <p class="text-muted mb-0">Notificami quando una persona che seguo pubblica un nuovo post.</p>
                             </div>
-                            <div class="col-auto">
-                                <input type="checkbox" class="custom-control-input settings-item" id="alert-new-post-person" checked="false" disabled />
+                            <div class="col-auto form-check form-switch">
+                                <input type="checkbox" role="switch" class="custom-control-input settings-item form-check-input" id="alert-new-post-person" checked="false" disabled />
                             </div>
                         </div>
                     </div>
                     <div class="list-group-item">
                         <div class="row align-items-center">
                             <div class="col">
-                                <label class="mb-0 fw-bolder" for="alert-new-post-animal">Post animali che seguo</label>
+                                <label class="mb-0 fw-bolder form-check-label" for="alert-new-post-animal">Post animali che seguo</label>
                                 <p class="text-muted mb-0">Notificami quando un animale che seguo pubblica un nuovo post.</p>
                             </div>
-                            <div class="col-auto">
-                                <input type="checkbox" class="custom-control-input settings-item" id="alert-new-post-animal" checked="false" disabled />
+                            <div class="col-auto form-check form-switch">
+                                <input type="checkbox" role="switch" class="custom-control-input settings-item form-check-input" id="alert-new-post-animal" checked="false" disabled />
                             </div>
                         </div>
                     </div>
                     <div class="list-group-item">
                         <div class="row align-items-center">
                             <div class="col">
-                                <label class="mb-0 fw-bolder" for="alert-comment-reply">Risposte ai miei commenti</label>
+                                <label class="mb-0 fw-bolder form-check-label" for="alert-comment-reply">Risposte ai miei commenti</label>
                                 <p class="text-muted mb-0">Notificami quando qualcuno risponde a un mio commento.</p>
                             </div>
-                            <div class="col-auto">
-                                <input type="checkbox" class="custom-control-input settings-item" id="alert-comment-reply" checked="false" disabled />
+                            <div class="col-auto form-check form-switch">
+                                <input type="checkbox" role="switch" class="custom-control-input settings-item form-check-input" id="alert-comment-reply" checked="false" disabled />
                             </div>
                         </div>
                     </div>
