9c9c82393b654daf41b0b1a16d4d81bd8169d296
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2023-06-02 14:24:34 +0200

    Created result-bar template

---
+++ src/template/result-bar.php
@@ -0,0 +1,9 @@
+<a href="<?php echo $href ?>">
+    <div class="card result-bar">
+        <div class="card-body p-2">
+            <div class="result-element"> <!-- img and label on same line to avoid empty space -->
+                <img class="miniature" src="<?php echo IMG_DIR . $img ?>" alt="Immagine profilo di <?php echo $username ?>" /><span class="fs-4 miniature-text"><?php echo $username ?></span>
+            </div>
+        </div>
+    </div>
+</a>
