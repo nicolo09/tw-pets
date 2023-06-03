d089305b38d4396d9eab6482fbe8f24049f74733
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2023-06-03 15:31:09 +0200

    Started working on cookie policy page

---
+++ src/cookie.php
@@ -0,0 +1,6 @@
+<?php
+require_once("bootstrap.php");
+$templateParams["title"]="Informativa dei cookie";
+$templateParams["page"] = "cookie-detail.php";
+require_once("template/base.php");
+?>
---
+++ src/template/cookie-detail.php
@@ -0,0 +1,5 @@
+<div class="container-fluid text-center">
+    <h1>Informativa dei cookie</h1>
+    <p>Questo sito utilizza cookie tecnici per facilitare la navigazione del sito e mantenere informazioni di login.</p>
+    <!--TODO-->
+</div>
