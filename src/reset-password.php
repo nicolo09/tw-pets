46551c61078b918413b48fda7da2ef28e5eff560
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2023-06-04 18:31:50 +0200

    Started working on password reset page

---
+++ src/reset-password.php
@@ -0,0 +1,35 @@
+<?php
+require_once("bootstrap.php");
+
+# Se l'utente è già loggato, viene reindirizzato alla home
+if (login_check($dbh)) {
+    header("Location: home.php");
+    exit;
+}
+
+# Se è stato inviato il form di reset-password
+/*
+TODO
+if (isset($_POST['username'], $_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $result = loginUser($username, $password, $dbh);
+    if ($result[0] == true) {
+        // Login eseguito con successo
+        header("Location: home.php");
+        exit;
+    } else {
+        // Login fallito
+        $templateParams["errors"] = $result[1];
+    }
+}
+
+if (isset($_GET["password_changed"]) && $_GET["password_changed"] == true) {
+    $templateParams["messages"][] = "Password cambiata con successo. Effettua nuovamente il login";
+}*/
+
+$templateParams["title"] = "PETS - Hai dimenticato la password";
+$templateParams["page"] = "reset-password-template.php";
+require_once("template/base-outside.php");
+
+?>
