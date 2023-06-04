4fc3f6af77ef8c9df258db3336c1e211f6ba1205
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2023-06-04 18:32:16 +0200

    Written cookie-detail.php

--- src/template/cookie-detail.php
+++ src/template/cookie-detail.php
@@ -1,5 +1,24 @@
-<div class="container-fluid text-center">
-    <h1>Informativa dei cookie</h1>
-    <p>Questo sito utilizza cookie tecnici per facilitare la navigazione del sito e mantenere informazioni di login.</p>
-    <!--TODO-->
+<div class="container">
+    <h1 class="text-center">Informativa dei cookie</h1>
+    <div class="mt-4">
+        <h3>Avviso sull'utilizzo dei cookie di sessione</h3>
+        <p>Questo sito web utilizza cookie di sessione al fine di migliorare la tua esperienza di navigazione. Desideriamo fornirti informazioni dettagliate sui cookie, sul loro utilizzo e sulla tua possibilità di gestirli in modo consapevole.</p>
+    </div>
+    <div class="mt-3">
+        <h3>Cosa sono i cookie di sessione?</h3>
+        <p>I cookie di sessione sono piccoli file di testo memorizzati sul tuo dispositivo durante la visita al nostro sito. Essi sono temporanei e vengono automaticamente eliminati quando chiudi il browser. Questi cookie non raccolgono informazioni personali e non possono identificarti.</p>
+    </div>
+    <div class="mt-3">
+        <h3>Come utilizziamo i cookie di sessione?</h3>
+        <p>I cookie di sessione sono essenziali per il corretto funzionamento del nostro sito web. Attraverso di essi, possiamo offrirti una navigazione sicura e fluida, garantendo la corretta visualizzazione delle pagine e consentendoti di accedere alle diverse sezioni del sito. Questi cookie ci permettono inoltre di tracciare le tue azioni durante la sessione, ad esempio per mantenere attiva la tua sessione di accesso o per memorizzare temporaneamente i dati inseriti nei formulari.</p>
+    </div>
+    <div class="mt-3">
+        <h3>Quali sono le tue scelte?</h3>
+        <p>La maggior parte dei browser accetta automaticamente i cookie di sessione. Tuttavia, hai la possibilità di gestirli attraverso le impostazioni del tuo browser. Puoi scegliere di accettare i cookie solo da determinati siti, bloccarli completamente o essere avvisato quando un cookie viene inviato al tuo dispositivo. Tieni presente che disabilitare i cookie di sessione ti impedirà di navigare sul nostro sito, dato che risultano necessari per la navigazione.</p>
+    </div>
+    <div class="mt-3">
+        <h3>Accettazione dei cookie</h3>
+        <p>La tua accettazione dei cookie di sessione avviene implicitamente attraverso la continuazione della navigazione sul nostro sito. Inoltre, ti informiamo che, utilizzando le funzionalità del sito, fornendo informazioni o interagendo con esso, acconsenti all'utilizzo dei cookie di sessione come descritto in questo avviso.</p>
+    </div>
+    <p class="mt-3 text-center fw-bold">Grazie per la comprensione e per l'utilizzo del nostro sito web.</p>
 </div>
