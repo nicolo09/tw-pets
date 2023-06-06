12eb7f7e54beba0b1fbf3d02aa38c3042e0f9bcf
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2023-06-06 11:24:57 +0200

    Formatting changes and added password change

--- src/new-password-reset.php
+++ src/new-password-reset.php
@@ -8,15 +8,29 @@ if (login_check($dbh)) {
 }
 
 //http://localhost/tw-pets/new-password-reset.php?id=".$code."
-if(isset($_GET["id"])){
-    if(isPasswordResetCodeValid($_GET["id"], $dbh)){
+if (isset($_GET["id"])) {
+    if (isPasswordResetCodeValid($_GET["id"], $dbh)) {
         //Il codice è valido
-        $code=$_GET["id"];
+        $code = $_GET["id"];
         //Permetti il reset della password
-        $username=$dbh->getUser($dbh->getResetCodeInfo($code)["email"])[0]["username"];
-        $templateParams["username"]=$username;
-        
-    }else{
+        $username = $dbh->getUser($dbh->getResetCodeInfo($code)["email"])[0]["username"];
+        $templateParams["username"] = $username;
+        //Il codice è valido e sto cercando di cambiare password
+        if (isset($_POST["new-password"], $_POST["new-password-repeat"])) {
+            $new_password = $_POST["new-password"];
+            $confirm_password = $_POST["new-password-repeat"];
+            $result = changePasswordReset($username, $new_password, $confirm_password, $dbh);
+            if ($result[0] == true) {
+                // Password cambiata con successo
+                // Rimuovi tutte le richieste di cambio password
+                header("Location: login.php?password_changed=true");
+                exit;
+            } else {
+                // Cambio password fallito
+                $templateParams["errors"] = $result[1];
+            }
+        }
+    } else {
         //Il codice non è valido
         $_SESSION["error"] = "Il codice non è più valido. Potrebbe essere scaduto, oppure ne è stato richiesto un altro. Riprova";
         header("Location: reset-password.php");
