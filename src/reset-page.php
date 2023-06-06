676793b2e52cdbad0d90291e18d52e4765a1d99a
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2023-06-06 10:45:01 +0200

    Changed reset-page

--- src/reset-page.php
+++ src/reset-page.php
@@ -1,27 +1,39 @@
-<h1>Stai cambiando la password per l'account <?php if (isset($templateParams["username"])) echo $templateParams["username"]; ?></h1>
-<div class="list-group-item">
-    <div class="row align-items-center">
-        <div class="col">
-            <label class="mb-0 fw-bolder" for="new-password">Nuova password</label>
-            <p class="text-muted mb-0">Inserisci la nuova password.</p>
-        </div>
-        <div class="col-auto">
-            <input type="password" id="new-password" name="new-password" autocomplete="new-password" />
-        </div>
-    </div>
-</div>
-<div class="list-group-item">
-    <div class="row align-items-center">
-        <div class="col">
-            <label class="mb-0 fw-bolder" for="new-password-repeat">Ripeti nuova password</label>
-            <p class="text-muted mb-0">Inserisci di nuovo la nuova password.</p>
-        </div>
-        <div class="col-auto">
-            <input type="password" id="new-password-repeat" name="new-password-repeat" autocomplete="new-password" />
-        </div>
-    </div>
-</div>
+<section class="text-center text-lg-start">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="card-body text-center">
+                    <h2 class="fw-bold">Inserisci la nuova password per l'account di <?php if (isset($templateParams["username"])) echo $templateParams["username"]; ?></h2>
+                    <form action="reset-password.php" method="post">
 
-<!-- Submit button -->
-<label>Ti manderemo una mail per resettare la password legata all'account scelto</label>
-<button type="submit" class="btn btn-primary btn-block mb-4">Invia mail di reset password</button>
+                        <!-- Password input -->
+                        <div class="row align-items-center">
+                            <div class="col">
+                                <label class="mb-0 fw-bolder" for="new-password">Nuova password</label>
+                                <p class="text-muted mb-0">Inserisci la nuova password.</p>
+                            </div>
+                            <div class="col-auto">
+                                <input type="password" id="new-password" name="new-password" autocomplete="new-password" />
+                            </div>
+                        </div>
+                        <div class="row align-items-center">
+                            <div class="col">
+                                <label class="mb-0 fw-bolder" for="new-password-repeat">Ripeti nuova password</label>
+                                <p class="text-muted mb-0">Inserisci di nuovo la nuova password.</p>
+                            </div>
+                            <div class="col-auto">
+                                <input type="password" id="new-password-repeat" name="new-password-repeat" autocomplete="new-password" />
+                            </div>
+                        </div>
+
+                        <!-- Submit button -->
+                        <button type="submit" class="btn btn-primary btn-block mb-4">Cambia password</button>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <h1 class="text-center">Placeholder immagine</h1>
+            </div>
+        </div>
+    </div>
+</section>
