f59553f73ba73f839c1ea66df631df19d8003365
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2023-06-06 18:05:41 +0200

    Added template to list followed profiles

---
+++ src/template/view-followed.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12 col-lg-10 col-xl-8 mx-auto">
+            <h1 class="text-center">Profili che segui</h1>
+            <div id="profile-list">
+                <div class="d-flex justify-content-center align-items-center mt-4" id="spinner">
+                    <div class="spinner-border text-primary spinner-border-sm" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="js/view-followed.js"></script>
