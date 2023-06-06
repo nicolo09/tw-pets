8d0d563c8fa5d9ed8669af919ab87eccc80fceac
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2023-06-06 18:04:22 +0200

    Added page to handle ajax request for followed profiles

---
+++ src/followed.php
@@ -0,0 +1,20 @@
+<?php
+require_once("bootstrap.php");
+
+if (!isUserLoggedIn($dbh)) {
+    header("Location: login.php");
+    exit;
+}
+
+//AJAX request
+if (isset($_GET["offset"]) && isset($_GET["number"])){
+    $results = $dbh->getFollowedProfiles(getUserName($dbh), $_GET["offset"], $_GET["number"]);
+
+    foreach($results as $user) {
+        $username = $user["username"];
+        $img = $user["immagine"];
+        $type = isset($user["email"])?"person":"animal";
+        $href = getProfileHref($username, $type);
+        require("template/result-bar.php");
+    }
+}
