c74efbee910787ca04d1c85ab107b98a3c49b1bd
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2023-06-06 18:05:15 +0200

    Added javascript to lazy load followed profiles

---
+++ src/js/view-followed.js
@@ -0,0 +1,26 @@
+const numberToFetch = 10;
+
+const intersectionObserver = new IntersectionObserver(entries => {
+    if (entries[0].intersectionRatio != 0) {
+        $.ajax({
+            url: "followed.php",
+            type: "GET",
+            data: {
+                offset: $("#profile-list").children().length,
+                number: numberToFetch,
+            },
+            success: function (data) {
+                $("#profile-list").append(data);
+                if (data == "") {
+                    $("#spinner").remove();
+                    if ($("#profile-list").children().length == 0) {
+                        $("#profile-list").append($("<h2>").addClass("text-center").text("Nessun profilo seguito"));
+                    }
+                }
+
+            }
+        });
+    }
+});
+
+intersectionObserver.observe(document.getElementById("spinner"));
