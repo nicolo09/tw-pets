f13095d1dc3e913d3d032d9b63772e49968e3dfd
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2023-06-08 11:59:16 +0200

    Fixed saved post javascript not loading post buttons and info

--- src/js/favorite-posts.js
+++ src/js/favorite-posts.js
@@ -7,6 +7,15 @@ function attachSaveButtonsEventListeners() {
     });
 }
 
+function attachPostButtons() {
+    const cards = document.querySelectorAll('[id^="post-card-"]');
+    cards.forEach(card => {
+        //Gets the post id
+        const id = card.id.split("-")[2];
+        attachStyleAndEventListeners(id);
+    });
+}
+
 const spinnerPostsLazyLoadObserver = new IntersectionObserver(entries => {
     if (entries[0].intersectionRatio != 0) {
         $.ajax({
@@ -21,9 +30,13 @@ const spinnerPostsLazyLoadObserver = new IntersectionObserver(entries => {
                     spinnerPostsLazyLoadObserver.unobserve($("#posts-spinner").get(0));
                     //Hide spinner
                     $("#posts-spinner").remove();
+                    if ($("#post-list").children().length == 0) {
+                        $("#post-list").append("<h2 class='text-center'>Nessun post salvato</h2>");
+                    }
                 } else {
                     $('#post-list').append(data);
                     attachSaveButtonsEventListeners();
+                    attachPostButtons();
                 }
             }
         })
@@ -32,3 +45,4 @@ const spinnerPostsLazyLoadObserver = new IntersectionObserver(entries => {
 
 spinnerPostsLazyLoadObserver.observe($("#posts-spinner").get(0));
 attachSaveButtonsEventListeners();
+attachPostButtons();
