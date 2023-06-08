faaaa820f83ed56363f756cc0f0caa48278cce92
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2023-06-08 12:54:08 +0200

    Fixed spinner not working in followed profile list

--- src/js/view-followed.js
+++ src/js/view-followed.js
@@ -11,7 +11,7 @@ const intersectionObserver = new IntersectionObserver(entries => {
             },
             success: function (data) {
                 $("#profile-list").append(data);
-                if (data == "") {
+                if (data == "" || $('<div></div>').html(data).children().length < numberToFetch) {
                     $("#spinner").remove();
                     if ($("#profile-list").children().length == 0) {
                         $("#profile-list").append($("<h2>").addClass("text-center").text("Nessun profilo seguito"));
